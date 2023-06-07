25126cbfffac1249584248a4a9c763bf2dc5232d
Raphael <raphael.mello@portaldomedico.com>
2023-06-07 14:35:43 -0300

    Adicionar arquivos de projeto.

---
+++ Contact Management/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Contact_Management.Models
+{
+    public class Contact
+    {
+        [DatabaseGenerated(databaseGeneratedOption:.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MinLength(5, ErrorMessage = "Name length can't be less than 5 characters.")]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(9, ErrorMessage = "Contact length can't be more than 9 digits.")]
+        public int ContactNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
